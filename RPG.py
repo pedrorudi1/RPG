cd2a2cc331d96738ae988f512675932d75c92d6a
pedrorudi1 <pedro_rudi@hotmail.com>
2024-09-19 14:48:49 -0300

    Sistema de lvlup adicionado

--- RPG.py
+++ RPG.py
@@ -1,7 +1,8 @@
 class Personagem:
-    def __init__(self, Nome, Classe, HP, Atk, MagAtk, Def, MagDef, Spd):
+    def __init__(self, Nome, Classe, level, HP, Atk, MagAtk, Def, MagDef, Spd):
         self.Nome = Nome
         self.Classe = Classe
+        self.level = level
         self.HitPoints = HP
         self.Atk = Atk
         self.MagAtk = MagAtk
@@ -27,10 +28,21 @@ class Personagem:
         self.Spd = self.Spd + Escudos.Spd
         print(f'Equipou {Escudos.Nome}')
     
+    def level_up(self, level):
+        self.level = level + 1
+        self.HitPoints = self.HitPoints + (2 * level)
+        self.Atk = self.Atk + (2 * level)
+        self.MagAtk = self.MagAtk + (1 * level)
+        self.Def = self.Def + (1 * level)
+        self.MagDef = self.MagDef + (1 * level)
+        self.Spd = self.Spd + (1 * level)
+        print(f'\n        Subiu {level} nível(eis)!')
+
     def __str__(self):
         return f'''
         Nome: {self.Nome}
         Classe: {self.Classe}
+        Nível: {self.level}
         HP: {self.HitPoints}
         Ataque: {self.Atk}
         Ataque Mágico: {self.MagAtk}
@@ -93,7 +105,7 @@ class Items:
         return f'{self.Efeito}'
         
 
-barbaro1 = Personagem("Pedro", "Mago", 12, 10, 2, 3, 5, 4)
+barbaro1 = Personagem("Pedro", "Mago", 1, 12, 10, 2, 3, 5, 4)
 ragnarok = Armas("Ragnarok", 20, 15, 10)
 excalibur = Armas("Excalibur", 22, 20, 15)
 potion = Items("Poção", "Recupera 5 HP")
@@ -101,12 +113,13 @@ armadura = Armaduras("Libra", 20, 20, 0)
 escudo = Escudos("Dragon Shield", 10, 8, 0)
 
 print(barbaro1)
-print(ragnarok)
-print(excalibur)
-print(armadura)
-print(escudo)
-barbaro1.Equipar_arma(excalibur)
-barbaro1.Equipar_armadura(armadura)
-barbaro1.Equipar_escudo(escudo)
+# print(ragnarok)
+# print(excalibur)
+# print(armadura)
+# print(escudo)
+# barbaro1.Equipar_arma(excalibur)
+# barbaro1.Equipar_armadura(armadura)
+# barbaro1.Equipar_escudo(escudo)
+barbaro1.level_up(99)
 print(barbaro1)
-print(potion)
+# print(potion)
